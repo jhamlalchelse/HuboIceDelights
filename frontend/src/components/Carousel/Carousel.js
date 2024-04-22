9cfdc698cb7627dbf3fde61aa5d72b77f9b9ae88
Jhamlal Chelse <jhamlalchelse@gmail.com>
2024-04-22 17:09:12 +0530

    carousel img

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import img1 from '../../assets/images/img1.jpeg';
-import img2 from '../../assets/images/img2.jpeg';
-import img3 from '../../assets/images/img3.jpeg';
-import './CarouselStyle.css';
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+import "./CarouselStyle.css";
+import { images } from "./constants";
 
 const CarouselDelights = () => {
   const carouselStyle = {
-    axis: 'vertical',
+    axis: "horizontal",
     infiniteLoop: true,
     autoPlay: true,
   };
   return (
     <Carousel className="carousel-container" {...carouselStyle}>
-      <div style={{ height: '500px' }}>
-        <img src={img1} alt="legend 1" className="w-full h-full object-fill" />
-        <div className="legend legend1">
-          <h4>Heading</h4>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, nihil!</p>
-        </div>
-      </div>
-      <div style={{ height: '500px' }}>
-        <img src={img2} alt="legend 2" className="w-full h-full object-fill" />
-        {/* <p className="legend">Legend 2</p> */}
-      </div>
-      <div style={{ height: '500px' }}>
-        <img src={img3} alt="legend 3" className="w-full h-full object-fill" />
-        {/* <p className="legend">Legend 3</p> */}
-      </div>
+      {images.map((item, index) => {
+        return (
+          <div key={index} style={{ height: "500px" }}>
+            <img
+              src={item.url}
+              alt={item.alt}
+              className="w-full h-full object-fill"
+            />
+            <div className="legend legend1">
+              <h4>{item.heading}</h4>
+              <p>{item.para}</p>
+            </div>
+          </div>
+        );
+      })}
     </Carousel>
   );
 };
---
+++ frontend/src/components/Carousel/constants.js
@@ -0,0 +1,20 @@
+export const images = [
+  {
+    url: "assets/images/carousel/img1.jpeg",
+    alt: "img1",
+    heading: "Heading",
+    para: " Lorem ipsum, dolor sit amet consectetur adipisicing elit eprehenderit, nihil!",
+  },
+  {
+    url: "assets/images/carousel/img2.jpeg",
+    alt: "img2",
+    heading: "Heading",
+    para: " Lorem ipsum, dolor sit amet consectetur adipisicing elit eprehenderit, nihil!",
+  },
+  {
+    url: "assets/images/carousel/img3.jpeg",
+    alt: "img3",
+    heading: "Heading",
+    para: " Lorem ipsum, dolor sit amet consectetur adipisicing elit eprehenderit, nihil!",
+  },
+];
